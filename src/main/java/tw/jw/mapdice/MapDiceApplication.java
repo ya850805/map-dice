5994cd68ae49e67b6b1be00779659927c37e85c2
Jason <ya850805@gmail.com>
2023-02-16 12:33:14 +0800

    Initial commit

---
+++ src/main/java/tw/jw/mapdice/MapDiceApplication.java
@@ -0,0 +1,13 @@
+package tw.jw.mapdice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapDiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapDiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tw/jw/mapdice/MapDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.jw.mapdice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapDiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
