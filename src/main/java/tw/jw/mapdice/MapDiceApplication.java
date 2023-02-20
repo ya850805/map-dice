7e0f1f2ba8c24e2135238b35c6ad24edffce2987
Jason <ya850805@gmail.com>
2023-02-20 15:18:14 +0800

    docs: Remove TODO

--- src/main/java/tw/jw/mapdice/MapDiceApplication.java
+++ src/main/java/tw/jw/mapdice/MapDiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class MapDiceApplication {
-    //TODO clear user dice count in redis every day.
     public static void main(String[] args) {
         SpringApplication.run(MapDiceApplication.class, args);
     }
