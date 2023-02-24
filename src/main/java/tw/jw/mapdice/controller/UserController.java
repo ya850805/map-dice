e691e3facd7e9f9df42a6ce220ecd6eded4dc0a1
Jason <ya850805@gmail.com>
2023-02-24 17:52:18 +0800

    feat: Add get current login username api.

--- src/main/java/tw/jw/mapdice/controller/UserController.java
+++ src/main/java/tw/jw/mapdice/controller/UserController.java
@@ -1,21 +1,34 @@
 package tw.jw.mapdice.controller;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tw.jw.mapdice.exception.MapDiceException;
 import tw.jw.mapdice.model.Response;
 import tw.jw.mapdice.model.UsersCreateRequest;
 import tw.jw.mapdice.service.UsersService;
+import tw.jw.mapdice.utils.JwtUtils;
 
 @RequestMapping("/users")
 @RestController
 public class UserController {
     @Autowired
     private UsersService usersService;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @GetMapping("/{jwt}")
+    public Response<String> getLoginUserName(@PathVariable("jwt") String jwt) {
+        Claims claims = jwtUtils.getClaimByToken(jwt);
+        if(jwtUtils.isTokenExpired(claims)) {
+            throw new JwtException("token is expired...");
+        } else {
+            return Response.ok(claims.getSubject());
+        }
+    }
 
     @PostMapping("/create")
     public Response<Integer> create(@RequestBody UsersCreateRequest users) {
--- src/main/java/tw/jw/mapdice/security/AuthenticationSuccess.java
+++ src/main/java/tw/jw/mapdice/security/AuthenticationSuccess.java
@@ -2,7 +2,6 @@ package tw.jw.mapdice.security;
 
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
