0413ce07f00598140944a7c8d98ad5869ee01d60
Jason <ya850805@gmail.com>
2023-02-26 15:01:06 +0800

    feat: Add get place_id field from Google nearby search API.

--- src/main/java/tw/jw/mapdice/model/PlaceResponse.java
+++ src/main/java/tw/jw/mapdice/model/PlaceResponse.java
@@ -1,11 +1,19 @@
 package tw.jw.mapdice.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class PlaceResponse {
+    @JsonProperty("place_id")
+    private String placeId;
+
     private String name;
+
     private Double rating;
+
+    @JsonProperty("user_ratings_total")
     private Integer userRatingsTotal;
+
     private String vicinity;
 }
