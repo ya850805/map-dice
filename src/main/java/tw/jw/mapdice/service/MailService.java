0b282bd3f8acec309c46ffcb4f517621bfe99510
Jason <ya850805@gmail.com>
2023-03-23 00:06:22 +0800

    feat: Add mail service send html function.

--- src/main/java/tw/jw/mapdice/service/MailService.java
+++ src/main/java/tw/jw/mapdice/service/MailService.java
@@ -1,5 +1,8 @@
 package tw.jw.mapdice.service;
 
+import javax.mail.MessagingException;
+
 public interface MailService {
     void send(String to, String title, String content);
+    void sendHtml(String to, String title, String content) throws MessagingException;
 }
--- src/main/java/tw/jw/mapdice/service/impl/MailServiceImpl.java
+++ src/main/java/tw/jw/mapdice/service/impl/MailServiceImpl.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import tw.jw.mapdice.service.MailService;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class MailServiceImpl implements MailService {
@@ -21,6 +25,17 @@ public class MailServiceImpl implements MailService {
         message.setTo(to);
         message.setSubject(title);
         message.setText(content);
+        javaMailSender.send(message);
+    }
+
+    @Override
+    public void sendHtml(String to, String title, String content) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(title);
+        helper.setText(content, true);
         javaMailSender.send(message);
     }
 }
