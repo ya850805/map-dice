ed3521dbf23be0fde0ecff3586e4f5db9f17e1c1
Jason <ya850805@gmail.com>
2023-04-04 00:09:12 +0800

    feat: Add place schema.

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS `users`;
 DROP TABLE IF EXISTS `users_collect`;
+DROP TABLE IF EXISTS `place`;
 
 CREATE TABLE `users` (
      `id` INT NOT NULL AUTO_INCREMENT,
@@ -14,4 +15,12 @@ CREATE TABLE `users_collect` (
      `user_id` INT NOT NULL,
      `place_id` VARCHAR(200) NOT NULL,
      PRIMARY KEY(id)
-)
+);
+
+CREATE TABLE `place` (
+    `id` VARCHAR(50) NOT NULL,
+    `name` VARCHAR(200) NOT NULL,
+    `url` VARCHAR(200) NOT NULL,
+    `website` VARCHAR(200),
+    `address` VARCHAR(200)
+);
