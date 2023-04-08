e830af42807788c807e72de3444af372120234cc
Jason <ya850805@gmail.com>
2023-04-09 00:42:46 +0800

    feat: Add request validation when create users_collect.

--- src/main/java/tw/jw/mapdice/controller/UsersCollectController.java
+++ src/main/java/tw/jw/mapdice/controller/UsersCollectController.java
@@ -1,15 +1,21 @@
 package tw.jw.mapdice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tw.jw.mapdice.exception.MapDiceException;
 import tw.jw.mapdice.model.Response;
 import tw.jw.mapdice.model.UserCollectCreateRequest;
 import tw.jw.mapdice.service.UsersCollectService;
 import tw.jw.mapdice.service.UsersService;
+
+import javax.validation.Valid;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users-collect")
@@ -21,7 +27,12 @@ public class UsersCollectController {
     private UsersService usersService;
 
     @PostMapping("/create")
-    public Response<Integer> create(@RequestBody UserCollectCreateRequest request) {
+    public Response<Integer> create(@Valid @RequestBody UserCollectCreateRequest request, BindingResult br) {
+        if(br.hasErrors()) {
+            String errorMessages = br.getAllErrors().stream().map(e -> e.getDefaultMessage()).collect(Collectors.joining(","));
+            throw new MapDiceException(HttpStatus.INTERNAL_SERVER_ERROR.value(), errorMessages);
+        }
+
         String username =  (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Integer userId = usersService.getByName(username).getId();
         service.create(userId, request.getPlaceId());
--- src/main/java/tw/jw/mapdice/model/UserCollectCreateRequest.java
+++ src/main/java/tw/jw/mapdice/model/UserCollectCreateRequest.java
@@ -2,7 +2,10 @@ package tw.jw.mapdice.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class UserCollectCreateRequest {
+    @NotBlank(message = "place id cannot be blank.")
     private String placeId;
 }
