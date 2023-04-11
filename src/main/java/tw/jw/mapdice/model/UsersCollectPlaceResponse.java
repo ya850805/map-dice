9290ee0a43dc88b404627a4c5d9a5c0747582603
Jason <ya850805@gmail.com>
2023-04-12 00:59:11 +0800

    feat: Add users_collect place response.

---
+++ src/main/java/tw/jw/mapdice/model/UsersCollectPlaceResponse.java
@@ -0,0 +1,14 @@
+package tw.jw.mapdice.model;
+
+import lombok.Data;
+
+@Data
+public class UsersCollectPlaceResponse {
+    private String name;
+
+    private String url;
+
+    private String website;
+
+    private String address;
+}
