e46c27dcac0db7294f413467d5bd326d00054fa1
Jason <ya850805@gmail.com>
2023-04-14 00:03:56 +0800

    feat: Add find all user collect places api and fix request url.

--- src/main/java/tw/jw/mapdice/controller/UsersCollectController.java
+++ src/main/java/tw/jw/mapdice/controller/UsersCollectController.java
@@ -1,17 +1,20 @@
 package tw.jw.mapdice.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import tw.jw.mapdice.exception.MapDiceException;
-import tw.jw.mapdice.model.Response;
-import tw.jw.mapdice.model.UsersCollectCreateDeleteRequest;
+import tw.jw.mapdice.model.*;
+import tw.jw.mapdice.service.PlaceService;
 import tw.jw.mapdice.service.UsersCollectService;
 import tw.jw.mapdice.service.UsersService;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
@@ -23,7 +26,10 @@ public class UsersCollectController {
     @Autowired
     private UsersService usersService;
 
-    @PostMapping("/create")
+    @Autowired
+    private PlaceService placeService;
+
+    @PostMapping("/")
     public Response<Integer> create(@Valid @RequestBody UsersCollectCreateDeleteRequest request, BindingResult br) {
         if(br.hasErrors()) {
             String errorMessages = br.getAllErrors().stream().map(e -> e.getDefaultMessage()).collect(Collectors.joining(","));
@@ -36,7 +42,7 @@ public class UsersCollectController {
         return Response.ok(1);
     }
 
-    @DeleteMapping("/delete")
+    @DeleteMapping("/")
     public Response<Integer> delete(@Valid @RequestBody UsersCollectCreateDeleteRequest request, BindingResult br) {
         if(br.hasErrors()) {
             String errorMessages = br.getAllErrors().stream().map(e -> e.getDefaultMessage()).collect(Collectors.joining(","));
@@ -47,5 +53,20 @@ public class UsersCollectController {
         Integer userId = usersService.getByName(username).getId();
         service.delete(userId, request.getPlaceId());
         return Response.ok(1);
+    }
+
+    @GetMapping("/")
+    public Response<List<UsersCollectPlaceResponse>> findByUser() {
+        String username =  (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Integer userId = usersService.getByName(username).getId();
+
+        List<String> placeIds = service.findByUserId(userId);
+        List<UsersCollectPlaceResponse> responses = placeService.findByIds(placeIds).stream().map(p -> {
+            UsersCollectPlaceResponse res = new UsersCollectPlaceResponse();
+            BeanUtils.copyProperties(p, res);
+            return res;
+        }).collect(Collectors.toList());
+
+        return Response.ok(responses);
     }
 }
